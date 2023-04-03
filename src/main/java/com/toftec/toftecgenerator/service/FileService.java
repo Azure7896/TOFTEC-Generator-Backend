2e34e9bc4e956f6672b322c5b5eaf2356c6cf448
Szymon Napora <95986791+Azure7896@users.noreply.github.com>
2023-04-03 02:08:04 +0200

    Update FileService.java

--- src/main/java/com/toftec/toftecgenerator/service/FileService.java
+++ src/main/java/com/toftec/toftecgenerator/service/FileService.java
@@ -1,2 +1,16 @@
-package com.toftec.toftecgenerator.service;public class FileService {
+package com.toftec.toftecgenerator.service;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+@Service
+public class FileService {
+
+    @Async
+    public void deleteFileAfterPdfGeneration(String filePath) throws InterruptedException {
+        Thread.sleep(5000);
+        File file = new File(filePath);
+        file.delete();
+    }
 }
